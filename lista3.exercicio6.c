3f1f3d71851fe704380a913c87424647ddf63e84
Luiz Vaz <luizfern137@gmail.com>
2026-09-16 08:09:39 -0300

    Create lista3.exercicio6.c

---
+++ lista3.exercicio6.c
@@ -0,0 +1,29 @@
+/*Implemente a função maiores, que recebe como parâmetro um vetor de números inteiros (vet) de
+tamanho n e um valor x. A função deve retornar quantos números maiores do que x existem nesse
+vetor. Essa função deve obedecer ao protótipo:
+int maiores(int n, int * vet, int x);*/
+#include <stdio.h>
+int maiores(int n, int * vet, int x);
+int main(){
+    int num, param;
+    printf("Digite o numero de elementos no vetor: \n");
+    scanf("%i", &num);
+    printf("Digite os elementos do vetor: \n");
+    int vetor[num];
+    for(int i = 0; i < num; i++){
+        scanf("%i", &vetor[i]);
+    }
+    printf("Digite o parametro: \n");
+    scanf("%i", &param);
+    int num_maiores = maiores(num, vetor, param);
+    if(num_maiores == 0)printf("Nao existem numeros maiores!");
+    else printf("Existem %i maiores que %i", num_maiores, param);
+    return 0;
+}
+int maiores(int n, int * vet, int x){
+    int n_maior = 0;
+    for(int i = 0; i < n ;i++){
+        if(x < vet[i]) n_maior++;
+    }
+    return n_maior;
+}
